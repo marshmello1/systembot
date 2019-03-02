747cc72f26da7fbe0f859e680e03ebd7f8d748dc
marshmello1 <44791226+marshmello1@users.noreply.github.com>
2019-03-03 00:44:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['ahelp','Type ahelp'];
+    var setGame = ['%help','Bot By B.N#0009'];
     var i = -1;
     var j = 0;
     setInterval(function (){
